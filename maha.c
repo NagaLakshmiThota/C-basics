10056507ee5fee776852627799b15ef48b5badc5
NagaLakshmiThota <96563961+NagaLakshmiThota@users.noreply.github.com>
2021-12-22 22:28:40 -0800

    Add files via upload

---
+++ maha.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+	int a,area,perimeter;
+	printf("enter the value of a");
+	scanf("%d",&a);
+	area=a*a;
+	perimeter=4*a;
+	printf("\n area=%d,perimeter=%d",area,perimeter);
+}
